e031e1a27b9996a8ad86658802b94d88d73bcdf1
rsn8887 <rsn8887@users.noreply.github.com>
2018-04-07 00:05:00 -0500

    vita: keep textures in cpu memory for faster reads

--- vita/i_video.c
+++ vita/i_video.c
@@ -868,7 +868,7 @@ static void SetVideoMode(void)
         // This has to be done after the SDL renderer is created because that inits vita2d.
         shader = Vita_SetShader(VSH_SHARP_BILINEAR_SIMPLE);
 
-        //vita2d_texture_set_alloc_memblock_type(SCE_KERNEL_MEMBLOCK_TYPE_USER_RW);
+        vita2d_texture_set_alloc_memblock_type(SCE_KERNEL_MEMBLOCK_TYPE_USER_RW);
     }
     else if (!strcmp(scaling_filter, "scale2x"))
     {
