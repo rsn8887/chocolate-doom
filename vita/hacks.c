98fd0e9eda81c6c13341cedafe0e6d8cf7fe3349
fgsfds <pvt.fgsfds@gmail.com>
2018-04-14 17:24:24 +0300

    vita: remove SDL_net-related hacks

--- vita/hacks.c
+++ vita/hacks.c
@@ -33,35 +33,6 @@ unsigned int _newlib_heap_size_user = 128 * 1024 * 1024;
 // vita-related initialization stuff
 // by the time this is executed myargc and myargv have already been set
 
-static void *net_memory = NULL;
-static int *net_errno = NULL;
-
-void Vita_InitNet(void)
-{
-    SceNetInitParam initparam;
-    int ret;
-
-    net_errno = sceNetErrnoLoc();
-
-    ret = sceNetShowNetstat();
-    if (ret == SCE_NET_ERROR_ENOTINIT)
-    {
-        net_memory = malloc(NET_INIT_SIZE);
-        initparam.memory = net_memory;
-        initparam.size = NET_INIT_SIZE;
-        initparam.flags = 0;
-        ret = sceNetInit(&initparam);
-        if (ret < 0) { free(net_memory); return; }
-    }
-
-    ret = sceNetCtlInit();
-    if (ret < 0)
-    {
-        sceNetTerm();
-        free(net_memory);
-    }
-}
-
 void Vita_Init(void)
 {
     sceAppUtilInit(&(SceAppUtilInitParam){}, &(SceAppUtilBootParam){});
@@ -70,77 +41,7 @@ void Vita_Init(void)
     scePowerSetBusClockFrequency(222);
     scePowerSetGpuClockFrequency(222);
     scePowerSetGpuXbarClockFrequency(166);
-    sceSysmoduleLoadModule(SCE_SYSMODULE_NET);
 
     mkdir(VITA_BASEDIR, 0755);
     mkdir(VITA_TMPDIR, 0755);
-
-    Vita_InitNet();
-}
-
-// SDL2_net looks for these and uses them as rand() and srand()
-// for some reason, and we don't have them in libc
-
-long int random(void)
-{
-  return rand();
-}
-
-void srandom(unsigned int seed)
-{
-  srand(seed);
-}
-
-// don't have these in our libc
-
-char *inet_ntoa(struct in_addr in)
-{
-  static char buf[32];
-  int ip;
-  ip = ntohl(in.s_addr);
-  snprintf(buf, sizeof(buf), "%d.%d.%d.%d",
-           (ip >> 24) & 0xff, (ip >> 16) & 0xff, (ip >> 8) & 0xff, ip & 0xff);
-  return buf;
-}
-
-in_addr_t inet_addr(const char *cp)
-{
-  int b1, b2, b3, b4;
-  int res;
-  res = sscanf(cp, "%d.%d.%d.%d", &b1, &b2, &b3, &b4);
-  if (res != 4) return (in_addr_t)(-1); // is actually expected behavior
-  return htonl((b1 << 24) | (b2 << 16) | (b3 << 8) | b4);
-}
-
-struct hostent *gethostbyaddr(const void *addr,
-                              socklen_t len, int type)
-{
-  static struct hostent ent;
-  static char sname[256];
-  static struct SceNetInAddr saddr;
-  static char *addrlist[2];
-  int rid, e;
-
-  addrlist[0] = (char *)&saddr;
-  addrlist[1] = NULL;
-
-  if (type != AF_INET || len != sizeof(uint32_t)) return NULL;
-
-  rid = sceNetResolverCreate("d_resolv", NULL, 0);
-  if (rid < 0) return NULL;
-
-  memcpy(&saddr.s_addr, addr, sizeof(uint32_t));
-
-  e = sceNetResolverStartAton(rid, &saddr, sname, sizeof(sname), 0, 0, 0);
-  sceNetResolverDestroy(rid);
-  if (e < 0) return NULL;
-
-  ent.h_name = sname;
-  ent.h_aliases = 0;
-  ent.h_addrtype = AF_INET;
-  ent.h_length = sizeof(struct SceNetInAddr);
-  ent.h_addr_list = addrlist;
-  ent.h_addr = addrlist[0];
-
-  return &ent;
 }
